02b683b1b7044236e774cf5627e5ce26fff476fa
Natalia Demakova <demakovanatalja@yandex.ru>
2023-11-20 12:45:12 +0400

    deploy1

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:demonat13.test-medods-form.git main:gh-pages
+git push -f git@github.com:demonat13/test-medods-form.git main:gh-pages
 
 cd -
